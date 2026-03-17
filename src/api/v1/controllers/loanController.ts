05299291d70c85a4a23e19f581dc25965a98758e
Lichao Huang <lhuang38@rrc.ca>
2026-03-17 14:03:20 -0500

    feat loancontroller

---
+++ src/api/v1/controllers/loanController.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from "express";
+import { loans } from "../data/loanData";
+import { successResponse } from "../models/responseModel";
+import { HTTP_STATUS } from "../../../constants/httpConstants";
+
+export const getAllLoans = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): void => {
+    try {
+        res.status(HTTP_STATUS.OK).json(successResponse(loans));
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getLoanById = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): void => {
+    try {
+        const { id } = req.params;
+
+        const loan = loans.find((l) => l.id === id);
+
+        if (!loan) {
+            res.status(HTTP_STATUS.NOT_FOUND).json({
+                message: "Loan not found",
+            });
+            return;
+        }
+
+        res.status(HTTP_STATUS.OK).json(successResponse(loan));
+    } catch (error) {
+        next(error);
+    }
+};
